4eb9bc76d9c1679b035db4736257dffb45c8517b
NoroMedafe <danya1515@inbox.ru>
2022-04-07 22:41:50 +0300

    Добавьте файлы проекта.

---
+++ inputControl/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inputControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stopWord = "exit";
+            string userWord;
+
+            while (true)
+            {
+                Console.Write("Введите любое слово: ");
+                userWord = Console.ReadLine();
+                userWord = userWord.ToLower();
+                if (userWord == stopWord)
+                    break;
+            }
+            Console.Clear();
+            Console.WriteLine("Вы ввели стоп слово");
+            Console.ReadKey();
+        }
+    }
+}
